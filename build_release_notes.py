cb7acec0e66c6eb7534d84f0cace03f6ebc79fa0
Yuji Wong <yuji@Yujidebijixingdiannao.local>
2023-12-22 11:19:53 +0800

    Add scripts and CI workflow

---
+++ build_release_notes.py
@@ -0,0 +1,181 @@
+import os
+
+
+class DevMessage:
+    message: str
+    commit_hash: str
+    url: str
+
+    def __init__(self, message: str, commit_hash: str, url: str):
+        self.message = message
+        self.commit_hash = commit_hash
+        self.url = url
+
+
+class Task:
+    id: str
+    title: str
+    url: str
+    dev_messages: list[DevMessage] = []
+
+    def __init__(self, id: str, title: str, url: str, dev_messages: list[DevMessage]):
+        self.id = id
+        self.title = title
+        self.url = url
+        self.dev_messages = dev_messages
+
+
+class Release:
+    version: str
+    tasks: list[Task] = []
+
+    def __init__(self, version: str, tasks: list[Task]):
+        self.version = version
+        self.tasks = tasks
+
+
+def version_block(version: str) -> str:
+    return ("""
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_section",
+                    "elements": [
+                        {
+                            "type": "emoji",
+                            "name": "android_robot"
+                        },
+                        {
+                            "type": "text",
+                            "text": " "
+                        },
+                        {
+                            "type": "text",
+                            "text": "${version}",
+                            "style": {
+                                "bold": true
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    """
+            .replace("${version}", version))
+
+
+def dev_message_element(dev_message: DevMessage) -> str:
+    return ("""
+    {
+        "type": "rich_text_section",
+        "elements": [
+            {
+                "type": "text",
+                "text": "${dev_message} ("
+            },
+            {
+                "type": "link",
+                "url": "${url}",
+                "text": "#${commit_hash}"
+            },
+            {
+                "type": "text",
+                "text": ")"
+            }
+        ]
+    }
+    """
+            .replace("${dev_message}", dev_message.message)
+            .replace("${url}", dev_message.url)
+            .replace("${commit_hash}", dev_message.commit_hash))
+
+
+def task_element(task: Task) -> str:
+    return ("""
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_list",
+                    "style": "bullet",
+                    "indent": 0,
+                    "border": 0,
+                    "elements": [
+                        {
+                            "type": "rich_text_section",
+                            "elements": [
+                                {
+                                    "type": "text",
+                                    "text": "${title} ["
+                                },
+                                {
+                                    "type": "link",
+                                    "url": "${url}",
+                                    "text": "ClickUp"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "]"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "rich_text_list",
+                    "style": "bullet",
+                    "indent": 1,
+                    "border": 0,
+                    "elements": [
+                        ${dev_messages}
+                    ]
+                }
+            ]
+        }
+    """
+            .replace("${title}", task.title)
+            .replace("${url}", task.url)
+            .replace("${dev_messages}",
+                     ",\n".join([dev_message_element(dev_message) for dev_message in task.dev_messages])))
+
+
+def build_slack_message(release: Release) -> str:
+    blocks = [version_block(release.version)]
+
+    for task in release.tasks:
+        blocks.append(task_element(task))
+
+    return """
+        {
+        "blocks": [
+            ${blocks}
+        ]
+    }
+    """.replace("${blocks}", ",\n".join(blocks))
+
+
+release = Release(
+    "v1.0.0",
+    [
+        Task("1", "Task 1", "https://google.com",   []),
+        Task("2", "Task 2", "https://google.com",   [
+            DevMessage("Message 1", "abc123", "https://google.com"),
+            DevMessage("Message 2", "abc123", "https://google.com"),
+            DevMessage("Message 3", "abc123", "https://google.com"),
+        ]),
+        Task("3", "Task 3", "https://google.com",   [
+            DevMessage("Message 1", "abc123", "https://google.com"),
+        ]),
+    ]
+)
+
+print(build_slack_message(release))
+
+# command = """
+# curl -X POST -H 'Content-type: application/json' --data '
+# """ + build_slack_message(release) + """
+#  ' https://hooks.slack.com/services/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
+# """
+#
+# os.system(command)
---
+++ generate_release_note.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python3
+from __future__ import annotations
+
+import argparse
+import json
+import re
+from datetime import datetime
+import requests
+import os
+
+from common.common import execute, get_current_highest_version_and_variant
+
+
+def parse_clickup_task_id_from_branch_name(branch_name: str) -> str | None:
+    if branch_name.startswith("CU-"):
+        task_id = re.split('[-_]', branch_name)[1]
+        return task_id
+    else:
+        return None
+
+def get_logs(branch: str, target_branch: str) -> str:
+    return execute(
+        f'git log {target_branch}..{branch}',
+        read_result=True,
+        dry_run=False,
+        verbose=False
+    )
+
+def parse_clickup_task_ids(logs: str) -> list[str]:
+    task_ids = []
+    for line in logs.splitlines():
+        task_id = line.strip()
+        if task_id.startswith("#"):
+            task_ids.append(task_id[1:])
+    return task_ids
+
+def get_clickup_token() -> str:
+    return os.environ["CLICKUP_TOKEN"]
+
+
+def get_clickup_task_by_id(task_id: str) -> dict:
+    return json.loads(
+        requests.get(
+            f'https://api.clickup.com/api/v2/task/{task_id}',
+            headers={
+                "Authorization": get_clickup_token()
+            }
+        ).text
+    )
+
+def get_nth_release_date_for_branch(branch, n, verbose):
+    date_string = execute(
+        f'git log {branch} --grep "build_" -n {n} --date=iso-strict | grep -oh "Date: .*"',
+        read_result=True,
+        dry_run=False,
+        verbose=verbose
+    ).splitlines()[n - 1]
+    return datetime.fromisoformat(date_string.removeprefix("Date:").strip()).isoformat(sep="T", timespec="auto")
+
+
+def get_latest_release_dates(verbose):
+    releases_on_beta = [get_nth_release_date_for_branch("release-beta", it, verbose) for it in [1, 2]]
+    releases_on_master = [get_nth_release_date_for_branch("master", it, verbose) for it in [1, 2]]
+    release_dates = releases_on_master + releases_on_beta
+    release_dates.sort(reverse=True)
+    return release_dates
+
+
+def get_clickup_link_based_on_branch(branch_name: str) -> str | None:
+    if branch_name.startswith("CU-"):
+        task_id = re.split('[-_]', branch_name)[1]
+        return f"https://app.clickup.com/t/{task_id}"
+    else:
+        return None
+
+
+def get_pr_descriptions_between_dates_from_dev(start, end, verbose=True):
+    gh_cli_result = execute(
+        f'gh pr list --search "is:closed merged:{start}..{end}" --json "body,number,baseRefName,headRefName"',
+        read_result=True,
+        dry_run=False,
+        verbose=verbose
+    )
+    all_prs = json.loads(gh_cli_result)
+
+    # headRefName to baseRefName
+    # This is for figuring out if the root branch is dev. For example: B->A->dev
+    merge_dict = {}
+    # build the merge_dict
+    for pr in all_prs:
+        merge_dict[pr['headRefName']] = pr['baseRefName']
+
+    prs = []
+    for pr in all_prs:
+        current_branch = pr['baseRefName']
+        while current_branch != 'dev':
+            try:
+                current_branch = merge_dict[current_branch]
+            except KeyError:
+                break
+        if current_branch == 'dev':
+            prs.append(pr)
+
+    if verbose:
+        [print(pr["number"]) for pr in prs]
+    return [(pr['body'], pr['headRefName']) for pr in prs]
+
+
+def get_release_note_entry_from_description(description: str, branch_name: str) -> str | None:
+    lines = description.splitlines()
+    if len(lines) < 2: return
+    release_note_header_line = lines[0]
+    release_note_entry_line = lines[1]
+    clickup_link = get_clickup_link_based_on_branch(branch_name)
+    if release_note_header_line.startswith("## Release Notes Entry") and not release_note_entry_line.startswith(">"):
+        if len(release_note_entry_line.strip()) > 0:
+            msg = f":white_small_square: {description.splitlines()[1].strip()}"
+            if clickup_link:
+                msg += f" (<{clickup_link}|ClickUp>)"
+            return msg
+
+
+# def get_args():
+#     parser = argparse.ArgumentParser(
+#         prog='generate_release_notes',
+#         description='Generate release notes from PR release note entries that are between the two most recent releases',
+#     )
+#     parser.add_argument(
+#         "-v", "--verbose", action="store_true"
+#     )
+#     args = parser.parse_args()
+#     return args.verbose
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog='generate_release_notes',
+        description='Generate release notes from PR release note entries that are between the two most recent releases',
+    )
+    parser.add_argument(
+        "branch", type=str, help="The branch to generate release notes for"
+    )
+    parser.add_argument(
+        "target_branch", type=str, help="The target branch to compare against"
+    )
+    args = parser.parse_args()
+    return args.branch, args.target_branch
+
+
+# def main():
+#     verbose = get_args()
+#     dates = get_latest_release_dates(verbose=verbose)
+#     descriptions = get_pr_descriptions_between_dates_from_dev(start=dates[1], end=dates[0], verbose=verbose)
+#     release_note_entries = [
+#         note for (desc, branch) in descriptions
+#         if (note := get_release_note_entry_from_description(desc, branch)) is not None
+#     ]
+#     if len(release_note_entries) > 0:
+#         version, variant = get_current_highest_version_and_variant(verbose)
+#         print(f"*v{version.strip()} {variant.strip()}* :steam_locomotive:")
+#         for entry in release_note_entries:
+#             print(entry)
+
+def main():
+    branch, target_branch = get_args()
+    logs = get_logs(branch, target_branch)
+    task_ids = set()
+    task_id_from_branch_name = parse_clickup_task_id_from_branch_name(branch)
+    if task_id_from_branch_name:
+        task_ids.add(task_id_from_branch_name)
+    t = parse_clickup_task_ids(logs)
+    task_ids.update(t)
+
+    for task_id in task_ids:
+        task = get_clickup_task_by_id(task_id)
+        print(task_id)
+        print(task["name"])
+        print(task["url"])
+        print("\n\n")
+
+
+if __name__ == '__main__':
+    main()
